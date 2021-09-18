5152f8ca1b8907f480e932e05c3d75c8a8a6b1b7
stefandur <stefandur@hotmail.com>
2021-09-19 00:18:18 +0200

    blinn-phong added

--- resources/shaders/2.model_lighting.fs
+++ resources/shaders/2.model_lighting.fs
@@ -35,19 +35,26 @@ vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
     float diff = max(dot(normal, lightDir), 0.0);
     // specular shading
     vec3 reflectDir = reflect(-lightDir, normal);
-    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    //float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     // attenuation
     float distance = length(light.position - fragPos);
     float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));
     // combine results
     vec3 ambient = light.ambient * vec3(texture(material.texture_diffuse1, TexCoords));
     vec3 diffuse = light.diffuse * diff * vec3(texture(material.texture_diffuse1, TexCoords));
-    vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
+    //vec3 specular = light.specular * spec * vec3(texture(material.texture_specular1, TexCoords).xxx);
     ambient *= attenuation;
     diffuse *= attenuation;
-    specular *= attenuation;
+    //specular *= attenuation;
+
+    float spec = 0.0;
+    vec3 halfwayDir = normalize(lightDir + viewDir);
+    spec = pow(max(dot(normal, halfwayDir), 0.0), 32.0);
+    vec3 specular = light.specular * spec;
+
     return (ambient + diffuse + specular);
 }
+
 
 void main()
 {
